3461bcbbc32cd653d91282012f8c44e98420c9ca
Bryce Hull <bahull@live.com>
2018-04-26 09:27:17 -0500

    Updated fs

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+
+const writeToFile = data => {
+  fs.writeFile("./data.json", JSON.stringify(data), err => {
+    if (err) return console.error(err);
+    process.exit();
+  });
+};

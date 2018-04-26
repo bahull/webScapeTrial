86b3d91ba3c66fd596a390fbf4346c680369ce23
Bryce Hull <bahull@live.com>
2018-04-26 09:37:51 -0500

    Updated with sleeper functions

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
+const sleep = require(`${__dirname}/utils`);
+
 const writeToFile = data => {
   fs.writeFile("./data.json", JSON.stringify(data), err => {
     if (err) return console.error(err);
     process.exit();
   });
 };
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/Main_Page");
+})();
